31373fb70c3058f47afd656c049404c5c673aa9f
Divesh2201 <diveshthakker2201@gmail.com>
2021-03-09 16:03:35 +0530

    footer and header

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
   return (
     <div className="App">
       {
-        token ? <Player /> : <Login />
+        token ? <Player spotify={spotify} /> : <Login />
       }
     </div>
   );
--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,46 @@
 import React from 'react'
 import './Footer.css'
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import { Grid, Slider } from "@material-ui/core";
+
 
 function Footer() {
   return (
     <div className='footer'>
-      <h1>I am footer</h1>
+      <div className='footer_left'>
+        <img className='footer_albumLogo' src="https://www.lyricsteam.com/wp-content/uploads/2020/10/titliaan-lyrics.jpg" alt='' />
+        <div className='footer_songInfo'>
+          <h4>Titilaayn!</h4>
+          <p>Afshza Khan</p>
+        </div>
+      </div>
+      <div className='footer_center'>
+        <ShuffleIcon className='footer_green'></ShuffleIcon>
+        <SkipPreviousIcon className='footer_icon'></SkipPreviousIcon>
+        <PlayCircleOutlineIcon fontSize='large' className='footer_icon'></PlayCircleOutlineIcon>
+        <SkipNextIcon className='footer_icon'></SkipNextIcon>
+        <RepeatIcon className='footer_green'></RepeatIcon>
+      </div>
+      <div className='footer_right'>
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider aria-labelledby='continuous-slider' />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   )
 }
